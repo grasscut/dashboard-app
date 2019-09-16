c22db59a75e588d3acd12f7f6b35b9e42f790210
thaisemanuele <thatitis@gmail.com>
2019-09-16 12:03:10 +0300

    making blogposts into cards

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -6,9 +6,11 @@
     <v-card-text class="pa-0" style="overflow: auto; margin: 0 16px 16px;">
       <v-progress-circular v-if="posts.length === 0" indeterminate class="progressSpinner"></v-progress-circular>
       <v-row>
-        <v-col v-for="post in posts">
-          <blog-post :post="post" :key="post.title"></blog-post>
-        </v-col>
+        <template v-for="post in posts">
+          <v-col sm="12" md="4" lg="4" :key="post.title">
+            <blog-post :post="post"></blog-post>
+          </v-col>
+        </template>
       </v-row>
       <v-btn v-if="posts.length > 0" text width="100%" @click="() => loadMore()" :loading="loadMoreProgress">Load more</v-btn>
     </v-card-text>
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,18 +1,22 @@
 <template>
-    <div style="display: inline-flex; width: 100%">
-        <v-list-item-avatar class="align-self-start">
+    <v-card height="300px">
+        <v-img height="20%"
+               width="100%"
+               class="white--text pa-0"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        ></v-img>
+        <v-avatar>
             <v-img :src="post.avatar"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
+        </v-avatar>
+        <v-card-text max-heigth="40%">
+            <a :href="post.items[0].url" target="_blank" style="text-decoration: none">{{ post.items[0].title }}</a>
             <b>{{ post.user }}</b>
-            <a :href="post.items[0].url" target="_blank">{{ post.items[0].title }}</a>
-        </v-list-item-content>
-        <v-list-item-action>
-            <v-list-item-action-text>
-                {{ moment(post.items[0].modifiedAt).calendar() }}
-            </v-list-item-action-text>
-        </v-list-item-action>
-    </div>
+        </v-card-text>
+        <v-subtitle-2>
+            {{ moment(post.items[0].modifiedAt).calendar() }}
+        </v-subtitle-2>
+
+    </v-card>
 </template>
 
 <script>
